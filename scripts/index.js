a34c49c0206299c1bce24b9719e2ca7c37f093be
Denis Bezukladnikov <denis3094@mail.ru>
2022-01-21 21:40:47 +0700

    исправить баги по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,28 @@ const popupContainer = document.querySelector('.popup__container');
 const popup = document.querySelector('.popup');
 const editBtn = document.querySelector('.profile__button-edit');
 const closeBtn = document.querySelector('.popup__close');
-// Находим форму в DOM
 let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
 let nameInput = document.querySelector('.popup__form_input_name');
 let jobInput = document.querySelector('.popup__form_input_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-
+//Функция открыть попап
 function openPopup() {
 	popup.classList.add('popup_opened');
 	nameInput.value = profileName.textContent;
 	jobInput.value = profileJob.textContent;
 }
-
-function closePopup(event) {
+//Функция закрыть попап
+function closePopup() {
 	popup.classList.remove('popup_opened');
 }
-
+//Функция закрыть попап при клике в любом месте
+function closePopupAnywhere(event) {
+	if (event.target === event.currentTarget) {
+		closePopup();
+	}
+}
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -41,11 +44,7 @@ function formSubmitHandler(evt) {
 
 editBtn.addEventListener('click', openPopup);
 closeBtn.addEventListener('click', closePopup);
-popup.addEventListener('click', function (event) {
-	if (event.target === event.currentTarget) {
-		closePopup();
-	}
-});
+popup.addEventListener('click', closePopupAnywhere);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
