9df75bae1c3da8a28104805517838bbdd79cdd4e
Denis Bezukladnikov <denis3094@mail.ru>
2022-01-28 20:46:31 +0700

    feat: сделать кнопку лайк

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,22 @@
 const body = document.querySelector('.page');
 const popupContainer = body.querySelector('.popup__container');
-const editPopup = body.querySelector('.popup_edit');
-const addPopup = body.querySelector('.popup_add');
+const popupEditProfile = body.querySelector('.popup_edit-profile');
+const popupAddCard = body.querySelector('.popup_add-card');
 const editBtn = body.querySelector('.profile__button-edit');
 const addBtn = body.querySelector('.profile__button-add');
-const closeBtnEditProfile = body.querySelector('.popup__close_edit_profile');
-const closeBtnAddCards = body.querySelector('.popup__close_add_cards');
-const formElement = body.querySelector('.popup__form');
+const closeBtnEditProfile = body.querySelector('.popup__close_edit-profile');
+const closeBtnAddCard = body.querySelector('.popup__close_add-card');
+const formEditProfile = body.querySelector('.popup__form_edit-profile');
+const formaddCard = body.querySelector('.popup__form_add-card');
 const nameInput = body.querySelector('.popup__input_type_name');
 const jobInput = body.querySelector('.popup__input_type_job');
 const profileName = body.querySelector('.profile__name');
 const profileJob = body.querySelector('.profile__job');
 const cardsItems = body.querySelector('.cards__items');
 const cardsTemplate = body.querySelector('.cards-template').content;
+const titleInput = body.querySelector('.popup__input_type_title');
+const linkInput = body.querySelector('.popup__input_type_link');
+const cardTitle = body.querySelector('.cards__title');
 
 
 //Функция открыть попап универсальная
@@ -22,7 +26,7 @@ function openPopup(popup) {
 
 //Функция открыть попап редактирования профиля
 function editProfileOpenPopupHandler() {
-	openPopup(editPopup);
+	openPopup(popupEditProfile);
 	nameInput.value = profileName.textContent;
 	jobInput.value = profileJob.textContent;
 }
@@ -34,33 +38,23 @@ function closePopup(popup) {
 
 //Функция закрыть попап редактирования профиля
 function editProfileClosePopupHandler() {
-	closePopup(editPopup);
+	closePopup(popupEditProfile);
 }
 
-//Функция закрыть попап при клике в любом месте и клавишей Escape
-//function closePopupAnywhere(event) {
-//	if (event.target === event.currentTarget || (event.code === 'Escape')) {
-//		closePopup(event.target);
-//	}
-//}
-
-
 //Функция открыть попап добавления карточек
-function addCardsOpenPopupHandler() {
-	openPopup(addPopup);
+function addCardOpenPopupHandler() {
+	openPopup(popupAddCard);
 }
 
 //Функция закрыть попап добавления карточек
-function addCardsClosePopupHandler() {
-	closePopup(addPopup);
+function addCardClosePopupHandler() {
+	closePopup(popupAddCard);
 }
-
-
 
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler(evt) {
+function formEditProfileSubmitHandler(evt) {
 	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 	// Так мы можем определить свою логику отправки.
 	// О том, как это делать, расскажем позже.
@@ -69,11 +63,22 @@ function formSubmitHandler(evt) {
 	// Вставьте новые значения с помощью textContent
 	profileName.textContent = nameInput.value;
 	profileJob.textContent = jobInput.value;
-	closePopup();
+	closePopup(popupEditProfile);
 }
 
 
-///Функция добавления карточек на страницу
+function formAddCardSubmitHandler(evt) {
+	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+	// Так мы можем определить свою логику отправки.
+	// О том, как это делать, расскажем позже.
+	// Получите значение полей jobInput и nameInput из свойства value
+	// Выберите элементы, куда должны быть вставлены значения полей
+	// Вставьте новые значения с помощью textContent
+	closePopup(popupAddCard);
+}
+
+
+///Функция добавления карточек на страницу из массива
 initialCards.forEach(function (element) {
 
 	const cardElement = cardsTemplate.cloneNode(true);
@@ -81,16 +86,22 @@ initialCards.forEach(function (element) {
 	cardElement.querySelector('.cards__title').textContent = element.name;
 	cardElement.querySelector('.cards__img').src = element.link;
 	cardElement.querySelector('.cards__img').setAttribute('alt', `${element.name}`);
+
+	///Лайк карточек на странице
+	cardElement.querySelector('.cards__button-like').addEventListener('click', function (evt) {
+		evt.target.classList.toggle('cards__button-like_active');
+	});
+
 	cardsItems.append(cardElement);
 });
 
 
+
 editBtn.addEventListener('click', editProfileOpenPopupHandler);
-addBtn.addEventListener('click', addCardsOpenPopupHandler);
+addBtn.addEventListener('click', addCardOpenPopupHandler);
 closeBtnEditProfile.addEventListener('click', editProfileClosePopupHandler);
-closeBtnAddCards.addEventListener('click', addCardsClosePopupHandler);
-editPopup.addEventListener('click', closePopupAnywhere);
-body.addEventListener('keyup', closePopupAnywhere);
+closeBtnAddCard.addEventListener('click', addCardClosePopupHandler);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formEditProfile.addEventListener('submit', formEditProfileSubmitHandler);
+formaddCard.addEventListener('submit', formAddCardSubmitHandler);
