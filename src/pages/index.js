f7e414f725985bfd12db1ea7265e985f56c4de11
Denis Bezukladnikov <denis3094@mail.ru>
2022-03-25 21:33:04 +0700

    feat: рефакторинг кода

---
+++ src/pages/index.js
@@ -0,0 +1,98 @@
+import {
+    initialCards,
+    configValidate,
+    editBtn,
+    nameInput,
+    jobInput,
+    formEditProfile,
+    addBtn,
+    formAddCard,
+    cardItems
+} from "../utils/constants.js";
+import {FormValidator} from "../components/FormValidator.js";
+import {Card} from "../components/Card.js";
+import {Section} from "../components/Section.js";
+import {PopupWithImage} from "../components/PopupWithImage.js";
+import {PopupWithForm} from "../components/PopupWithForm.js";
+import {UserInfo} from "../components/UserInfo.js";
+
+
+//Функция открыть попап редактирования профиля
+function openEditProfilePopup() {
+    editProfileValidator.resetErrors();
+    const {name, job} = userInfo.getUserInfo();
+    nameInput.value = name;
+    jobInput.value = job;
+    editProfileValidator.toggleButtonState();
+    editProfilePopup.open();
+}
+
+//Функция редактирования профиля
+function submitEditProfile(data) {
+    const {name, about_me} = data;
+    userInfo.setUserInfo(name, about_me);
+    editProfilePopup.close();
+}
+
+//Функция открыть попап добавления карточек
+function openAddCardPopup() {
+    addCardValidator.resetErrors();
+    addCardValidator.toggleButtonState();
+    addCardPopup.open();
+}
+
+//Функция вставки карточек
+const prependCard = (cardElement) => {
+    cardItems.prepend(cardElement);
+}
+
+//Функция создания карточек
+const createCard = (data) => {
+    const card = new Card(data, '.cards-template', () => {
+        imagePopup.open(data.name, data.link);
+    });
+    const cardElement = card.createCard();
+
+    return cardElement;
+}
+
+//Функция рендера карточек
+const renderCard = (data) => {
+    prependCard(createCard(data));
+}
+
+
+//Слушатели кнопок модалок
+editBtn.addEventListener('click', openEditProfilePopup);
+addBtn.addEventListener('click', openAddCardPopup);
+
+
+//Валидация модалок
+const editProfileValidator = new FormValidator(configValidate, formEditProfile);
+const addCardValidator = new FormValidator(configValidate, formAddCard);
+
+editProfileValidator.enableValidation();
+addCardValidator.enableValidation();
+
+//Рендер карточек на странице
+const section = new Section({items: initialCards, renderer: renderCard}, '.cards__items');
+
+section.renderItems();
+
+//Привязка модалок
+const imagePopup = new PopupWithImage('.popup_pic');
+const addCardPopup = new PopupWithForm('.popup_add-card', (data) => {
+    section.addItem(createCard(data));
+    addCardPopup.close();
+});
+const editProfilePopup = new PopupWithForm('.popup_edit-profile', submitEditProfile);
+
+imagePopup.setEventListeners();
+addCardPopup.setEventListeners();
+editProfilePopup.setEventListeners();
+
+//Заполнение информации профиля
+const userInfo = new UserInfo({profileNameSelector: '.profile__name', profileJobSelector: '.profile__job'});
+
+
+
---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+export const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+},
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    },
+];
+
+export const configValidate = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+}
+
+export const editBtn = document.querySelector('.profile__button-edit');
+export const nameInput = document.querySelector('.popup__input_type_name');
+export const jobInput = document.querySelector('.popup__input_type_job');
+export const formEditProfile = document.querySelector('.popup__form_edit-profile');
+export const addBtn = document.querySelector('.profile__button-add');
+export const formAddCard = document.querySelector('.popup__form_add-card');
+export const cardItems = document.querySelector('.cards__items');
